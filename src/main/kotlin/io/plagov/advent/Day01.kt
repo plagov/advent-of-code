e09a6e5f5b8a3504afe5cb46f27670e81b745782
Vitali Plagov <vplagov@pm.me>
2021-12-02 21:41:32 +0200

    Use extension function to zip list into pairs
    Thanks to the hint from Kotlin team at JetBrains, there's an extension
    function in the standard library that can zip the list of elements in
    the list, and it is possible to apply a predicate to this function.
    It returns a list of booleans. Then just count those that are `true`.

--- src/main/kotlin/io/plagov/advent/Day01.kt
+++ src/main/kotlin/io/plagov/advent/Day01.kt
@@ -2,17 +2,6 @@ package io.plagov.advent
 
 class Day01 {
 
-  fun numbersOfIncreases(input: List<String>): Int {
-    var accumulator = 0
-    val list = input.map { it.toInt() }
-
-    for (i in 1 until list.size) {
-      if (list[i] > list[i - 1]) {
-        accumulator++
-      }
-    }
-
-    return accumulator
-  }
-
+  fun numbersOfIncreases(input: List<String>) =
+    input.map { it.toInt() }.zipWithNext { a, b -> a < b }.count { it }
 }
