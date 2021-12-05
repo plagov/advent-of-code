6435e0b223eaa066711546d5cc77c3fb03fc4f28
Vitali Plagov <vplagov@pm.me>
2021-12-05 07:29:21 +0200

    Improve error message

--- src/main/kotlin/io/plagov/advent/Day02.kt
+++ src/main/kotlin/io/plagov/advent/Day02.kt
@@ -7,15 +7,14 @@ class Day02 {
     var horizontal = 0
     var depth = 0
     input.forEach { line ->
-      val (command, direction) = regex.find(line)?.destructured ?: error("Something went wrong")
+      val (command, direction) = regex.find(line)?.destructured
+        ?: error("Failed to parse the line '$line' with given regex $regex")
       when (command) {
         "forward" -> horizontal += direction.toInt()
         "down" -> depth += direction.toInt()
         "up" -> depth -= direction.toInt()
       }
     }
-    println("horizontal: $horizontal")
-    println("depth: $depth")
     return horizontal * depth
   }
 }
