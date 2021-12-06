acd68cf9ad7dda7ec94ceb0af601cd732741d59d
Vitali Plagov <vplagov@pm.me>
2021-12-06 21:41:29 +0200

    Solve task input

--- src/test/kotlin/io/plagov/advent/Day03Test.kt
+++ src/test/kotlin/io/plagov/advent/Day03Test.kt
@@ -9,8 +9,13 @@ class Day03Test : ShouldSpec({
   val day3 = Day03()
 
   val sampleInput = readInputFile("day03-sample.txt")
+  val taskInput = readInputFile("day03.txt")
 
-  should("find decimal number from binary gamma and epsilon rates for sample input") {
+  should("find decimal number from product of binary gamma and epsilon rates for sample input") {
     day3.productOfDecimalGammaAndEpsilonRates(sampleInput) shouldBe 198
+  }
+
+  should("find decimal number from product of binary gamma and epsilon rates for task input") {
+    day3.productOfDecimalGammaAndEpsilonRates(taskInput) shouldBe 3009600
   }
 })
