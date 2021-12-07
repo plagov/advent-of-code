98bbd88359a118eb5b5163799aa3070707fa7c61
Vitali Plagov <vplagov@pm.me>
2021-12-07 07:41:48 +0200

    Refactor by extracting methods

--- src/main/kotlin/io/plagov/advent/Day03.kt
+++ src/main/kotlin/io/plagov/advent/Day03.kt
@@ -35,23 +35,34 @@ class Day03 {
     }
 
   fun productOfDecimalOxygenAndCO2ratings(input: List<String>): Int {
+    val oxygen = findOxygenRate(input).joinToString("").toInt(2)
+    val co2Rating = findCO2Rate(input).joinToString("").toInt(2)
+    return oxygen * co2Rating
+  }
+
+  private fun findOxygenRate(input: List<String>): MutableList<String> {
     val indices = input.first().indices
     var oxygenList = input.toMutableList()
-    var co2List = input.toMutableList()
 
     for (index in indices) {
       val verticalList = convertToVerticalList(oxygenList)
-      val mostCommon = if (oxygenList.size == 2 && lastIndiciesAreEqual(oxygenList)) {
+      val mostCommon = if (oxygenList.size == 2 && lastIndicesAreEqual(oxygenList)) {
         "1"
       } else {
         mostCommonElement(verticalList)[index]
       }
       oxygenList = oxygenList.filter { it[index].toString() == mostCommon }.toMutableList()
     }
+    return oxygenList
+  }
+
+  private fun findCO2Rate(input: List<String>): MutableList<String> {
+    val indices = input.first().indices
+    var co2List = input.toMutableList()
 
     for (index in indices) {
       val verticalList = convertToVerticalList(co2List)
-      val lessCommon = if (co2List.size == 2 && lastIndiciesAreEqual(co2List)) {
+      val lessCommon = if (co2List.size == 2 && lastIndicesAreEqual(co2List)) {
         "0"
       } else {
         lessCommonElement(verticalList)[index]
@@ -59,12 +70,9 @@ class Day03 {
       co2List = co2List.filter { it[index].toString() == lessCommon }.toMutableList()
     }
 
-    val oxygen = oxygenList.joinToString("").toInt(2)
-    val co2Rating = co2List.joinToString("").toInt(2)
-
-    return oxygen * co2Rating
+    return co2List
   }
 
-  private fun lastIndiciesAreEqual(list: MutableList<String>) =
+  private fun lastIndicesAreEqual(list: MutableList<String>) =
     list.map { it.last() }.distinct().size == 2
 }
