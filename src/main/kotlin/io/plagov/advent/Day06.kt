65122db2fdeaa9ea14a01b620cf1135d4159752a
Vitali Plagov <vplagov@pm.me>
2021-12-15 23:49:55 +0200

    Try to improve operation with for-loop

--- src/main/kotlin/io/plagov/advent/Day06.kt
+++ src/main/kotlin/io/plagov/advent/Day06.kt
@@ -3,22 +3,17 @@ package io.plagov.advent
 class Day06 {
   fun partOneAndTwo(input: List<String>, days: Int): Long {
     val finalList = input.first().split(",").map { it.toInt() }.toMutableList()
-
     for (day in 1..days) {
-
-      val listIterator = finalList.listIterator()
-
-      while (listIterator.hasNext()) {
-        val number = listIterator.next().dec()
-        if (number == -1) {
-          listIterator.set(6)
-          listIterator.add(8)
-        } else {
-          listIterator.set(number)
+        for (i in 0 until finalList.size) {
+          val newNumber = finalList[i].dec()
+          if (newNumber == -1) {
+            finalList[i] = 6
+            finalList.add(8)
+          } else {
+            finalList[i] = newNumber
+          }
         }
-      }
     }
-
     return finalList.count().toLong()
   }
 }
