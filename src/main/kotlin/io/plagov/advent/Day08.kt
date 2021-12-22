cde8304bc834bc6485075b5bffc4f1ccb075fe32
Vitali Plagov <vplagov@pm.me>
2021-12-22 23:15:48 +0200

    Clean up and refactoring

--- src/main/kotlin/io/plagov/advent/Day08.kt
+++ src/main/kotlin/io/plagov/advent/Day08.kt
@@ -3,9 +3,7 @@ package io.plagov.advent
 class Day08 {
 
   fun partOne(input: List<String>): Int {
-    val mapOfEntries = parseInputToMapOfEntries(input)
-
-    val parsedOutputs = mapOfEntries.entries.map { it.value.split(" ") }
+    val parsedOutputs = parseInputToPatternAndOutput(input).entries.map { it.value.split(" ") }
 
     val outputsWithUniqueDigits = parsedOutputs.flatMap { outputPart ->
       outputPart.filter { singleOutPut -> singleOutPut.length in listOf(2, 4, 3, 7) }
@@ -15,62 +13,56 @@ class Day08 {
   }
 
   fun partTwo(input: List<String>): Int {
-    val mapOfEntries = parseInputToMapOfEntries(input)
-
-    return mapOfEntries.map { (pattern, output) ->
+    return parseInputToPatternAndOutput(input).map { (pattern, output) ->
       parseOutputByPattern(pattern, output)
     }.sum()
   }
 
   private fun parseOutputByPattern(rawPattern: String, rawOutput: String): Int {
     val output = rawOutput.split(" ")
-    val (a,b,c,d,e,f,g) = decodePattern(rawPattern)
+    val (a, b, c, d, e, f, g) = decodePattern(rawPattern)
 
-    // TODO should compare ignore order
-    return output.map { out ->
-      when(out.toList().sorted()) {
+    return output.joinToString("") { out ->
+      when (out.toList().sorted()) {
         listOf(a, b, c, e, f, g).sorted() -> "0"
         listOf(c, f).sorted() -> "1"
-        listOf(a,c,d,e,g).sorted() -> "2"
-        listOf(a,c,d,f,g).sorted() -> "3"
-        listOf(b,c,d,f).sorted() -> "4"
-        listOf(a,b,d,f,g).sorted() -> "5"
-        listOf(a,b,d,e,f,g).sorted() -> "6"
-        listOf(a,c,f).sorted() -> "7"
-        listOf(a,b,c,d,e,f,g).sorted() -> "8"
-        listOf(a,b,c,d,f,g).sorted() -> "9"
+        listOf(a, c, d, e, g).sorted() -> "2"
+        listOf(a, c, d, f, g).sorted() -> "3"
+        listOf(b, c, d, f).sorted() -> "4"
+        listOf(a, b, d, f, g).sorted() -> "5"
+        listOf(a, b, d, e, f, g).sorted() -> "6"
+        listOf(a, c, f).sorted() -> "7"
+        listOf(a, b, c, d, e, f, g).sorted() -> "8"
+        listOf(a, b, c, d, f, g).sorted() -> "9"
         else -> error("Something went wrong. Output: $out")
       }
-    }.joinToString("").toInt()
+    }.toInt()
   }
 
   private fun decodePattern(rawPattern: String): SevenSegmentDisplay {
     val pattern = rawPattern.split(" ")
 
-    val one = pattern.first { it.length == 2 }
-    val four = pattern.first { it.length == 4 }
-    val eight = pattern.first { it.length == 7 }
+    val one = pattern.first { it.length == 2 }.toSet()
+    val four = pattern.first { it.length == 4 }.toSet()
+    val eight = pattern.first { it.length == 7 }.toSet()
 
-    val digitA = pattern.filter { it.length in listOf(2, 3) }.map { it.toList() }.flatten().groupBy { it }
+    val a = pattern.filter { it.length in listOf(2, 3) }.flatMap { it.toList() }.groupBy { it }
       .filter { it.value.size == 1 }.entries.single().key
 
-    val CandDandE = pattern.filter { it.length == 6 }.map { it.toList() }.flatten().groupBy { it }
-      .filter { it.value.size == 2 }.entries.map { it.key }
+    val cde = pattern.filter { it.length == 6 }.flatMap { it.toList() }.groupBy { it }
+      .filter { it.value.size == 2 }.entries.mapTo(HashSet()) { it.key }
 
-    val digitC = CandDandE.intersect(one.toList()).single()
-    val digitF = one.toList().minus(digitC).single()
+    val c = cde.intersect(one).single()
+    val f = one.minus(c).single()
+    val d = four.minus(c).intersect(cde).single()
+    val b = four.minus(setOf(c, d, f)).single()
+    val e = cde.minus(setOf(c, d)).single()
+    val g = eight.minus(setOf(a, b, c, d, e, f)).single()
 
-    val digitD = four.toList().minus(digitC).intersect(CandDandE).single()
-    val digitB = four.toList().minus(listOf(digitC, digitD, digitF)).single()
-
-    val digitE = CandDandE.minus(listOf(digitC, digitD)).single()
-
-    val digitG = eight.toList().minus(listOf(digitA, digitB, digitC, digitD, digitE, digitF).toSet()).single()
-
-    return SevenSegmentDisplay(digitA, digitB, digitC, digitD, digitE, digitF, digitG)
+    return SevenSegmentDisplay(a, b, c, d, e, f, g)
   }
 
-  private fun parseInputToMapOfEntries(input: List<String>) = input.associate {
+  private fun parseInputToPatternAndOutput(input: List<String>) = input.associate {
     val (pattern, output) = it.split(""" | """)
     pattern to output
   }
