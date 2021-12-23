fa2db2a1b9dbb81be2fb2555a57fa8730144c8a3
Vitali Plagov <vplagov@pm.me>
2021-12-23 09:21:51 +0200

    Refactoring of day 9

--- src/main/kotlin/io/plagov/advent/Day09.kt
+++ src/main/kotlin/io/plagov/advent/Day09.kt
@@ -3,23 +3,26 @@ package io.plagov.advent
 class Day09 {
 
   fun partOne(input: List<String>): Int {
-    val heightmap = mutableListOf<List<Int>>()
+    val heightmap = buildList {
+      for (currentLineIndex in input.indices) {
+        val currentLine = input[currentLineIndex]
+        val previousLine = input.getOrNull(currentLineIndex - 1)
+        val nextLine = input.getOrNull(currentLineIndex + 1)
 
-    for (currentLineIndex in input.indices) {
-      val currentLine = input[currentLineIndex]
-      val previousLine = input.getOrNull(currentLineIndex - 1)
-      val nextLine = input.getOrNull(currentLineIndex + 1)
-      for (currentNumberIndex in currentLine.indices) {
-        val currentNumber = currentLine[currentNumberIndex]
-        val previousNumber = currentLine.getOrNull(currentNumberIndex - 1)
-        val nextNumber = currentLine.getOrNull(currentNumberIndex + 1)
-        val location = mutableListOf<Char>()
-        location.add(currentNumber)
-        if (previousNumber != null) location.add(previousNumber)
-        if (nextNumber != null) location.add(nextNumber)
-        if (previousLine != null) location.add(previousLine[currentNumberIndex])
-        if (nextLine != null) location.add(nextLine[currentNumberIndex])
-        heightmap.add(location.map { it.digitToInt() })
+        for (currentNumberIndex in currentLine.indices) {
+          val currentNumber = currentLine[currentNumberIndex]
+          val previousNumber = currentLine.getOrNull(currentNumberIndex - 1)
+          val nextNumber = currentLine.getOrNull(currentNumberIndex + 1)
+          val location = buildList {
+            add(currentNumber)
+            if (previousNumber != null) add(previousNumber)
+            if (nextNumber != null) add(nextNumber)
+            if (previousLine != null) add(previousLine[currentNumberIndex])
+            if (nextLine != null) add(nextLine[currentNumberIndex])
+          }
+
+          add(location.map { it.digitToInt() })
+        }
       }
     }
 
