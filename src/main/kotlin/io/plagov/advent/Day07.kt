b8bdc49963d089f81c8f4333c49c030b1fce1835
Vitali Plagov <vplagov@pm.me>
2021-12-23 06:44:03 +0200

    Day 7: build read-only map instead of mutable
    Use a builder function `buildMap` to create a read-only map instead of
    defining an empty mutable map and populating it later in the for loop.

--- src/main/kotlin/io/plagov/advent/Day07.kt
+++ src/main/kotlin/io/plagov/advent/Day07.kt
@@ -24,16 +24,17 @@ class Day07 {
   fun partTwo(input: List<String>): Int {
     val crabs = input.first().split(",").map { it.toInt() }
 
-    val fuelConsumption = mutableMapOf<Int, Int>()
     val maxPosition = crabs.maxOrNull() ?: error("The list is empty")
 
-    for (position in 1..maxPosition) {
-      var fuel = 0
-      for (crab in crabs) {
-        val steps = (crab - position).absoluteValue
-        fuel += (1..steps).sum()
+    val fuelConsumption = buildMap {
+      for (position in 1..maxPosition) {
+        var fuel = 0
+        for (crab in crabs) {
+          val steps = (crab - position).absoluteValue
+          fuel += (1..steps).sum()
+        }
+        put(position, fuel)
       }
-      fuelConsumption[position] = fuel
     }
 
     return fuelConsumption.minByOrNull { it.value }?.value ?: error("Couldn't sort the map")
