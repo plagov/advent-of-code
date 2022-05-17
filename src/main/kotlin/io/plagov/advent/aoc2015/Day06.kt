8f71a1a7b4fd4e29952fe93770551fb969b7dd01
Vitali Plagov <vplagov@pm.me>
2022-05-17 22:06:21 +0300

    Handle command by a single function

--- src/main/kotlin/io/plagov/advent/aoc2015/Day06.kt
+++ src/main/kotlin/io/plagov/advent/aoc2015/Day06.kt
@@ -15,48 +15,31 @@ class Day06 {
       val (command, start, _, end) = regex.find(instruction)?.destructured
         ?: error("Couldn't parse the instruction string: $instruction")
 
-      when (command) {
-        "turn on" -> turnLightsOn(start, end)
-        "turn off" -> turnLightsOff(start, end)
-        "toggle" -> toggleLights(start, end)
-        else -> error("Unknown command: $command")
-      }
-
+      processCommand(command, start, end)
     }
 
     return grid.flatMap { it.asIterable() }.count { it == 1 }
   }
 
-  private fun turnLightsOn(startPoint: String, endPoint: String) {
-    val (startX, startY) = startPoint.split(",").map { it.toInt() }
-    val (endX, endY) = endPoint.split(",").map { it.toInt() }
-    for (x in startX..endX) {
-      for (y in startY..endY) {
-        grid[x][y] = 1
-      }
-    }
-  }
+  private fun processCommand(command: String, start: String, end: String) {
+    val (startX, startY) = start.split(",").map { it.toInt() }
+    val (endX, endY) = end.split(",").map { it.toInt() }
 
-  private fun turnLightsOff(startPoint: String, endPoint: String) {
-    val (startX, startY) = startPoint.split(",").map { it.toInt() }
-    val (endX, endY) = endPoint.split(",").map { it.toInt() }
     for (x in startX..endX) {
       for (y in startY..endY) {
-        grid[x][y] = 0
-      }
-    }
-  }
 
-  private fun toggleLights(startPoint: String, endPoint: String) {
-    val (startX, startY) = startPoint.split(",").map { it.toInt() }
-    val (endX, endY) = endPoint.split(",").map { it.toInt() }
-    for (x in startX..endX) {
-      for (y in startY..endY) {
-        if (grid[x][y] == 0) {
-          grid[x][y] = 1
-        } else {
-          grid[x][y] = 0
+        when (command) {
+          "turn on" -> grid[x][y] = 1
+          "turn off" -> grid[x][y] = 0
+          "toggle" -> {
+            if (grid[x][y] == 0) {
+              grid[x][y] = 1
+            } else {
+              grid[x][y] = 0
+            }
+          }
         }
+
       }
     }
   }
