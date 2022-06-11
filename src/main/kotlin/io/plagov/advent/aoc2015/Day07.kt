b674c45a1fead4cdafc3ea40eea40231db253c9d
Vitali Plagov <vplagov@pm.me>
2022-06-11 22:55:11 +0300

    Extract every operation into separate method

--- src/main/kotlin/io/plagov/advent/aoc2015/Day07.kt
+++ src/main/kotlin/io/plagov/advent/aoc2015/Day07.kt
@@ -2,9 +2,25 @@ package io.plagov.advent.aoc2015
 
 class Day07 {
 
-  private val assignValueRegex = """^(\d+) -> (\w+)$""".toRegex()
-
   private var score = mutableMapOf<String, Int?>()
+
+  private val andOperationBetweenSignalsRegex = """^(\w+) AND (\w+) -> (\w+)$""".toRegex()
+
+  fun partOne(input: List<String>, searchingKey: String): Int {
+    initializeAllKeys(input)
+
+    while (score[searchingKey] == null) {
+      Signals(input)
+        .assignDigitsToKeys()
+        .doShiftOperations()
+        .doNotOperations()
+        .doOrOperations()
+        .doAndOperationsBetweenKeys()
+        .doAndOperationWithDigit()
+        .doAssignmentFromAnotherKey()
+    }
+    return score[searchingKey] ?: error("No value found for key '$searchingKey'")
+  }
 
   private fun initializeAllKeys(input: List<String>) {
     val reg = """(.*) -> (\w+)""".toRegex()
@@ -15,116 +31,125 @@ class Day07 {
     }
   }
 
-  fun partOne(input: List<String>, searchingKey: String): Int {
 
-    initializeAllKeys(input)
+  @JvmInline
+  private value class Signals(val value: List<String>)
 
-    while (score[searchingKey] == null) {
+  private fun Signals.assignDigitsToKeys(): Signals {
+    val assignValueRegex = """^(\d+) -> (\w+)$""".toRegex()
 
-      // do assignment with digits
-      input.filter { it.matches(assignValueRegex) }.forEach { cmd ->
-        val (value, target) = assignValueRegex.find(cmd)?.destructured ?: error("Error parsing")
+    this.value.filter { it.matches(assignValueRegex) }.forEach { cmd ->
+      val (value, target) = assignValueRegex.find(cmd)?.destructured ?: error("Error parsing")
+      if (score[target] == null) {
+        score[target] = value.toInt()
+      }
+    }
+    return this
+  }
+
+  private fun Signals.doShiftOperations(): Signals {
+    val knownKeysAfterAssignment = score.entries.filter { it.value != null }.map { it.key }
+    val shiftReg = """^(\w+) ([L|R]SHIFT) (\d+) -> (\w+)${'$'}""".toRegex()
+    this.value
+      .filter { it.matches(shiftReg) }
+      .filter { it.substringBefore(" ") in knownKeysAfterAssignment }
+      .forEach { cmd ->
+        val (left, operation, bitCount, target) = shiftReg.find(cmd)?.destructured ?: error("Error parsing")
+        val result = getShiftResult(operation, score[left]!!, bitCount.toInt())
         if (score[target] == null) {
-          score[target] = value.toInt()
+          score[target] = result
         }
       }
+    return this
+  }
 
-      // do shifting
-      val knownKeysAfterAssignment = score.entries.filter { it.value != null }.map { it.key }
-      val shiftReg = """^(\w+) ([L|R]SHIFT) (\d+) -> (\w+)${'$'}""".toRegex()
-      input
-        .filter { it.matches(shiftReg) }
-        .filter { it.substringBefore(" ") in knownKeysAfterAssignment }
-        .forEach { cmd ->
-          val (left, operation, bitCount, target) = shiftReg.find(cmd)?.destructured ?: error("Error parsing")
-          val result = getShiftResult(operation, score[left]!!, bitCount.toInt())
-          if (score[target] == null) {
-            score[target] = result
-          }
+  private fun Signals.doNotOperations(): Signals {
+    val knownKeysAfterTwoSteps = score.entries.filter { it.value != null }.map { it.key }
+    val notReg = """^NOT (\w+) -> (\w+)$""".toRegex()
+    this.value
+      .filter { it.matches(notReg) }
+      .filter {
+        val (left, _) = notReg.find(it)?.destructured ?: error("123")
+        left in knownKeysAfterTwoSteps
+      }
+      .forEach { cmd ->
+        val (left, target) = notReg.find(cmd)?.destructured ?: error("123")
+        if (score[target] == null) {
+          score[target] = score[left]?.xor(65535)
         }
+      }
+    return this
+  }
 
-      // do NOT
-      val knownKeysAfterTwoSteps = score.entries.filter { it.value != null }.map { it.key }
-      val notReg = """^NOT (\w+) -> (\w+)$""".toRegex()
-      input
-        .filter { it.matches(notReg) }
-        .filter {
-          val (left, _) = notReg.find(it)?.destructured ?: error("123")
-          left in knownKeysAfterTwoSteps
+  private fun Signals.doOrOperations(): Signals {
+    val knownKeysAfterThreeSteps = score.entries.filter { it.value != null }.map { it.key }
+    val orReg = """^(\w+) OR (\w+) -> (\w+)$""".toRegex()
+    this.value
+      .filter { it.matches(orReg) }
+      .filter {
+        val (left, right, _) = orReg.find(it)?.destructured ?: error("213")
+        knownKeysAfterThreeSteps.containsAll(listOf(left, right))
+      }
+      .forEach { cmd ->
+        val (left, right, target) = orReg.find(cmd)?.destructured ?: error("213")
+        if (score[target] == null) {
+          score[target] = score[left]!! or score[right]!!
         }
-        .forEach { cmd ->
-          val (left, target) = notReg.find(cmd)?.destructured ?: error("123")
-          if (score[target] == null) {
-            score[target] = score[left]?.xor(65535)
-          }
-        }
+      }
+    return this
+  }
 
-      // do OR
-      val knownKeysAfterThreeSteps = score.entries.filter { it.value != null }.map { it.key }
-      val orReg = """^(\w+) OR (\w+) -> (\w+)$""".toRegex()
-      input
-        .filter { it.matches(orReg) }
-        .filter {
-          val (left, right, _) = orReg.find(it)?.destructured ?: error("213")
-          knownKeysAfterThreeSteps.containsAll(listOf(left, right))
+  private fun Signals.doAndOperationsBetweenKeys(): Signals {
+    val knownKeysAfterFourSteps = score.entries.filter { it.value != null }.map { it.key }
+    this.value
+      .filter { it.matches(andOperationBetweenSignalsRegex) }
+      .filter {
+        val (left, right, _) = andOperationBetweenSignalsRegex.find(it)?.destructured ?: error("213")
+        knownKeysAfterFourSteps.containsAll(listOf(left, right))
+      }
+      .forEach { cmd ->
+        val (left, right, target) = andOperationBetweenSignalsRegex.find(cmd)?.destructured ?: error("213")
+        if (score[target] == null) {
+          score[target] = score[left]!! and score[right]!!
         }
-        .forEach { cmd ->
-          val (left, right, target) = orReg.find(cmd)?.destructured ?: error("213")
-          if (score[target] == null) {
-            score[target] = score[left]!! or score[right]!!
-          }
-        }
+      }
+    return this
+  }
 
-      // do AND
-      val knownKeysAfterFourSteps = score.entries.filter { it.value != null }.map { it.key }
-      val andReg = """^(\w+) AND (\w+) -> (\w+)$""".toRegex()
-      input
-        .filter { it.matches(andReg) }
-        .filter {
-          val (left, right, _) = andReg.find(it)?.destructured ?: error("213")
-          knownKeysAfterFourSteps.containsAll(listOf(left, right))
+  private fun Signals.doAndOperationWithDigit(): Signals {
+    val knownKeysAfterFiveSteps = score.entries.filter { it.value != null }.map { it.key }
+    val andWithDigitReg = """^(\d+) AND (\w+) -> (\w+)$""".toRegex()
+    this.value
+      .filter { it.matches(andWithDigitReg) }
+      .filter {
+        val (_, right, _) = andOperationBetweenSignalsRegex.find(it)?.destructured ?: error("213")
+        right in knownKeysAfterFiveSteps
+      }
+      .forEach { cmd ->
+        val (left, right, target) = andOperationBetweenSignalsRegex.find(cmd)?.destructured ?: error("213")
+        if (score[target] == null) {
+          score[target] = left.toInt() and score[right]!!
         }
-        .forEach { cmd ->
-          val (left, right, target) = andReg.find(cmd)?.destructured ?: error("213")
-          if (score[target] == null) {
-            score[target] = score[left]!! and score[right]!!
-          }
-        }
+      }
+    return this
+  }
 
-      // do AND with one digit
-      val knownKeysAfterFiveSteps = score.entries.filter { it.value != null }.map { it.key }
-      val andWithDigitReg = """^(\d+) AND (\w+) -> (\w+)$""".toRegex()
-      input
-        .filter { it.matches(andWithDigitReg) }
-        .filter {
-          val (_, right, _) = andReg.find(it)?.destructured ?: error("213")
-          right in knownKeysAfterFiveSteps
+  private fun Signals.doAssignmentFromAnotherKey(): Signals {
+    val knownKeysAfterSixSteps = score.entries.filter { it.value != null }.map { it.key }
+    val assignFromAnotherReg = """^(\w+) -> (\w+)$""".toRegex()
+    this.value
+      .filter { it.matches(assignFromAnotherReg) }
+      .filter {
+        val (source, _) = assignFromAnotherReg.find(it)?.destructured ?: error("123")
+        source in knownKeysAfterSixSteps
+      }
+      .forEach { cmd ->
+        val (source, target) = assignFromAnotherReg.find(cmd)?.destructured ?: error("Error parsing")
+        if (score[target] == null) {
+          score[target] = score[source]
         }
-        .forEach { cmd ->
-          val (left, right, target) = andReg.find(cmd)?.destructured ?: error("213")
-          if (score[target] == null) {
-            score[target] = left.toInt() and score[right]!!
-          }
-        }
-
-      // do assignment from another char
-      val knownKeysAfterSixSteps = score.entries.filter { it.value != null }.map { it.key }
-      val assignFromAnotherReg = """^(\w+) -> (\w+)$""".toRegex()
-      input
-        .filter { it.matches(assignFromAnotherReg) }
-        .filter {
-          val (source, _) = assignFromAnotherReg.find(it)?.destructured ?: error("123")
-          source in knownKeysAfterSixSteps
-        }
-        .forEach { cmd ->
-          val (source, target) = assignFromAnotherReg.find(cmd)?.destructured ?: error("Error parsing")
-          if (score[target] == null) {
-            score[target] = score[source]
-          }
-        }
-
-    }
-    return score[searchingKey] ?: error("No value found for key '$searchingKey'")
+      }
+    return this
   }
 
   private fun getShiftResult(operation: String, left: Int, bitCount: Int) =
