490388b154d7e5a57c9ce5c6c3a89938dbe33719
Vitali Plagov <vitali.plagov@topia.com>
2022-07-07 09:43:33 +0300

    WIP: trying to build all possible routes

--- src/main/kotlin/io/plagov/advent/aoc2015/Day09.kt
+++ src/main/kotlin/io/plagov/advent/aoc2015/Day09.kt
@@ -2,14 +2,85 @@ package io.plagov.advent.aoc2015
 
 class Day09 {
 
-  fun partOne(input: List<String>): Int {
-    val parsedRoutes = parseRoutes(input)
+  private lateinit var parsedRoutes: List<Route>
+  private var visitedRoutes = mutableListOf<Route>()
+  private lateinit var finalRoute: String
+  private var mapOfRoutes = mutableMapOf<Int, List<Route>>()
+  private var id = 0
+  private var distinctPoints = listOf<String>()
 
-    val distinctPoints = parsedRoutes.flatMap { route ->
+  fun partOne(input: List<String>): Int {
+    parsedRoutes = parseRoutes(input)
+
+    distinctPoints = parsedRoutes.flatMap { route ->
       listOf(route.from, route.to)
     }.distinct()
 
-    return 0
+    /**
+     * London
+     * Dublin
+     * Belfast
+     */
+
+    for (route in parsedRoutes) {
+      // do route.to first
+      visitedRoutes.add(route)
+      mapOfRoutes.put(id, listOf(route))
+      appendRouteTo(route)
+      visitedRoutes.clear()
+      id++
+
+      visitedRoutes.add(route)
+      mapOfRoutes.put(id, listOf(route))
+      appendRouteFrom(route)
+      visitedRoutes.clear()
+      id++
+    }
+
+    val shortest = mapOfRoutes.map { e ->
+      e.key to e.value.sumOf { it.distance }
+    }.minOf { it.second }
+
+    return shortest
+  }
+
+  private fun appendRouteFrom(route: Route) {
+    val potentialRoute = parsedRoutes.firstOrNull { it !in visitedRoutes } ?: return
+
+    if (mapOfRoutes[id]?.size!! < distinctPoints.size) { // todo condition is wrong, find another data model
+      if (route.from == potentialRoute.from) {
+        visitedRoutes.add(potentialRoute)
+        mapOfRoutes.put(id, mapOfRoutes[id]!!.plus(potentialRoute))
+        appendRouteFrom(potentialRoute)
+      } else if (route.from == potentialRoute.to) {
+        visitedRoutes.add(potentialRoute)
+        mapOfRoutes.put(id, mapOfRoutes[id]!!.plus(potentialRoute))
+        appendRouteFrom(potentialRoute)
+      } else {
+        visitedRoutes.add(potentialRoute)
+        appendRouteFrom(route)
+      }
+    }
+  }
+
+  private fun appendRouteTo(route: Route) {
+    val potentialRoute = parsedRoutes.firstOrNull { it !in visitedRoutes } ?: return
+
+    if (mapOfRoutes[id]?.size!! < distinctPoints.size) { // todo condition is wrong, find another data model
+      if (route.to == potentialRoute.from) {
+        visitedRoutes.add(potentialRoute)
+        mapOfRoutes.put(id, mapOfRoutes[id]!!.plus(potentialRoute))
+        appendRouteTo(potentialRoute)
+      } else if (route.to == potentialRoute.to) {
+        visitedRoutes.add(potentialRoute)
+        mapOfRoutes.put(id, mapOfRoutes[id]!!.plus(potentialRoute))
+        appendRouteTo(potentialRoute)
+      } else {
+        visitedRoutes.add(potentialRoute)
+        appendRouteTo(route)
+      }
+    }
+
   }
 
   private fun parseRoutes(input: List<String>): List<Route> {
--- src/test/kotlin/io/plagov/advent/aoc2015/Day09Test.kt
+++ src/test/kotlin/io/plagov/advent/aoc2015/Day09Test.kt
@@ -7,11 +7,18 @@ import util.readInputFile
 class Day09Test {
 
   private val sampleInput = readInputFile("2015/day09-sample.txt")
+  private val taskInput = readInputFile("2015/day09.txt")
 
   private val day9 = Day09()
 
   @Test
   fun solveFirst() {
-    assertEquals(-1, day9.partOne(sampleInput))
+    assertEquals(605, day9.partOne(sampleInput))
+//    assertEquals(-1, day9.partOne(taskInput))
   }
 }
+
+/**
+ * 278 - too high
+ * 172 - to high
+ */
