dcb649b92868b26386e35e413b51d43b82b0bcd4
Vitali Plagov <vplagov@pm.me>
2023-12-02 15:59:24 +0200

    Solve part 2 of day 1

--- src/main/kotlin/io/plagov/advent/aoc2023/Day01.kt
+++ src/main/kotlin/io/plagov/advent/aoc2023/Day01.kt
@@ -2,11 +2,47 @@ package io.plagov.advent.aoc2023
 
 class Day01 {
 
-  fun partOne(input: List<String>): Long {
-    return input.sumOf { line ->
-      val first = line.split("").first { it.matches("""\d""".toRegex()) }
-      val last = line.split("").last { it.matches("""\d""".toRegex()) }
-      "$first$last".toLong()
+  private val numbers = listOf(
+    Pair("one", "o1e"),
+    Pair("two", "t2o"),
+    Pair("three", "t3e"),
+    Pair("four", "f4r"),
+    Pair("five", "f5e"),
+    Pair("six", "s6x"),
+    Pair("seven", "s7n"),
+    Pair("eight", "e8t"),
+    Pair("nine", "n9e")
+  )
+
+  fun partOne(input: List<String>) = input.sumOf { line -> solve(line) }
+
+  fun partTwo(input: List<String>): Int {
+     return input.map { line ->
+      numbers.fold(line) { currentLine, (oldValue, newValue) ->
+        currentLine.replace(oldValue, newValue)
+      }
+    }.sumOf { line -> solve(line) }
+  }
+
+  private fun solve(line: String): Int {
+    val regex = Regex("""[1-9]""")
+    val first = regex.findAll(line).first().value.mapToDigits()
+    val last = regex.findAll(line).last().value.mapToDigits()
+    return "$first$last".toInt()
+  }
+
+  private fun String.mapToDigits(): String {
+    return when (this) {
+      "one" -> "1"
+      "two" -> "2"
+      "three" -> "3"
+      "four" -> "4"
+      "five" -> "5"
+      "six" -> "6"
+      "seven" -> "7"
+      "eight" -> "8"
+      "nine" -> "9"
+      else -> this
     }
   }
 }
--- src/test/kotlin/io/plagov/advent/aoc2023/Day01Test.kt
+++ src/test/kotlin/io/plagov/advent/aoc2023/Day01Test.kt
@@ -6,7 +6,8 @@ import util.readInputFile
 
 class Day01Test {
 
-  private val sampleInput = readInputFile("2023/day01-sample.txt")
+  private val sampleInputPartOne = readInputFile("2023/day01-p1-sample.txt")
+  private val sampleInputPartTwo = readInputFile("2023/day01-p2-sample.txt")
 
   private val taskInput = readInputFile("2023/day01.txt")
 
@@ -14,11 +15,21 @@ class Day01Test {
 
   @Test
   fun solveFirstForSampleInput() {
-    assertEquals(142, day1.partOne(sampleInput))
+    assertEquals(142, day1.partOne(sampleInputPartOne))
   }
 
   @Test
   fun solveFirstForTaskInput() {
     assertEquals(54338, day1.partOne(taskInput))
+  }
+
+  @Test
+  fun solveSecondForSampleInput() {
+    assertEquals(281, day1.partTwo(sampleInputPartTwo))
+  }
+
+  @Test
+  fun solveSecondForTaskInput() {
+    assertEquals(53389, day1.partTwo(taskInput))
   }
 }
