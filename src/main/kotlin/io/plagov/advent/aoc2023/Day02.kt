eb9629e1ffa9076dce28b344a7a62f88adaf346a
Vitali Plagov <vplagov@pm.me>
2023-12-09 12:17:07 +0200

    AoC 2023: day 2 part 1 (#52)
    * Solve part 1 of day 1
    
    * Solve part 2 of day 1
    
    * Day 2 part 1

---
+++ src/main/kotlin/io/plagov/advent/aoc2023/Day02.kt
@@ -0,0 +1,33 @@
+package io.plagov.advent.aoc2023
+
+class Day02 {
+
+  fun partOne(input: List<String>): Int {
+    val records = input.map { line ->
+      val result = """(Game \d+): (.*)""".toRegex().find(line)!!
+      val (game, sets) = result.destructured
+      val id = game.substringAfter("Game ").toInt()
+      val cubesSets = sets.split("; ").map { set ->
+        parseSet(set)
+      }
+      Record(id, cubesSets)
+    }
+    return records.filter { record ->
+      record.sets.none { it.red > 12 || it.green > 13 || it.blue > 14 }
+    }.sumOf { it.id }
+  }
+
+  private fun parseSet(set: String): CubesSet {
+    val redPattern = """(\d+ red)""".toRegex()
+    val greenPattern = """(\d+ green)""".toRegex()
+    val bluePattern = """(\d+ blue)""".toRegex()
+    val red = redPattern.find(set)?.value?.substringBefore(" ")?.toInt() ?: 0
+    val green = greenPattern.find(set)?.value?.substringBefore(" ")?.toInt() ?: 0
+    val blue = bluePattern.find(set)?.value?.substringBefore(" ")?.toInt() ?: 0
+    return CubesSet(blue, red, green)
+  }
+}
+
+data class Record(val id: Int, val sets: List<CubesSet>)
+
+data class CubesSet(val blue: Int, val red: Int, val green: Int)
---
+++ src/test/kotlin/io/plagov/advent/aoc2023/Day02Test.kt
@@ -0,0 +1,24 @@
+package io.plagov.advent.aoc2023
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import util.readInputFile
+
+class Day02Test {
+
+  private val sampleInput = readInputFile("2023/day02-sample.txt")
+
+  private val taskInput = readInputFile("2023/day02.txt")
+
+  private val day02 = Day02()
+
+  @Test
+  fun solveFirstForSampleInput() {
+    assertEquals(8, day02.partOne(sampleInput))
+  }
+
+  @Test
+  fun solveFirstForTaskInput() {
+    assertEquals(2416, day02.partOne(taskInput))
+  }
+}
